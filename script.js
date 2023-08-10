85c96025d1f6c125bf0a3a54b48188ed450a64c9
squendle <squendle@gmail.com>
2023-08-10 12:08:03 -0700

    Style buttons and create variables and click listener

--- script.js
+++ script.js
@@ -34,3 +34,39 @@ function operate(num1, operator, num2){
     return sub(num1, num2);
   }
 };
+
+// these are the variables for each button
+
+const zero = document.querySelector("#zero");
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
+const plus = document.querySelector("#plus");
+const minus = document.querySelector("#minus");
+const times = document.querySelector("#times");
+const divided = document.querySelector("divided");
+const clear = document.querySelector("#clear");
+const equals = document.querySelector("#equals");
+
+const buttons = document.querySelectorAll(".button");
+const display = document.querySelector(".display");
+
+let displayVal = "HI";
+
+buttons.forEach(button => {
+  button.addEventListener('click', displayUpdate());
+});
+// I want for each click on a button to take the id and grab the correct variable, then update the displayVal with that variable....
+
+// when a button clicks, that number goes into the display
+ 
+
+function displayUpdate(displayVal){
+  display.textContent = displayVal;
+};
