7c0c818f95184e31ecc9ba97fc5191c70ab327c7
squendle <squendle@gmail.com>
2023-08-11 12:09:58 -0700

    make display value an array of numbers to update display properly

--- script.js
+++ script.js
@@ -52,24 +52,30 @@ const display = document.querySelector("#display");
 
 //display updating stuff
 
-let displayVal = "0"
-display.textContent = displayVal;
+let defaultVal = 0;
+display.textContent = defaultVal;
+
+let displayVal = [];
 
 function displayUpdate(displayVal){
-  display.textContent = displayVal;
+  display.textContent = displayVal.join('');
 };
 
 buttons.forEach(button => {
   let value = button.value;
   button.addEventListener('click', () =>{
-    displayVal = value;
+    displayVal.push(value);
     displayUpdate(displayVal);
   });
 });
 
-//every button has an id
-//any button gets a click
-//that button's unique id should be added to a variable which updates the display in real time, concatenating with the next button click as well
+
 //the total value displayed gets stored in ANOTHER variable which will be used in operations
 //that final variable will need to be made into an integer before going into the operations functions
 
+// if the button clicked has a class of operator, the display should stop updating
+// the first displayed value should be stored in a variable to be used in the operate function
+// the operator button clicked should be stored as the operator for the function
+// the display starts a new update with the next input number
+// the process repeats for as many operators and numbers the user wants
+//  
